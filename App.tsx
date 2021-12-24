ab2d08ca3735b0d70c5a1462bbc63f49a5937254
Prescilla Lecurieux <prescilla.lecurieux@energisme.com>
2021-12-24 14:04:01 +0100

    add listitems & navigation

--- App.tsx
+++ App.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from './src/screens/HomeScreen';
+import {QuoteScreen} from './src/screens/QuoteScreen';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{backgroundColor: isDarkMode ? Colors.black : Colors.white}}
-        />
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'Home'} component={HomeScreen} />
+        <Stack.Screen name={'Quotes'} component={QuoteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
